118094ad5f213222cc37bb7574ba9a4aa15e9fba
Mihail Dragnev <bymykel5@gmail.com>
2022-03-28 19:01:13 +0200

    refactor: change font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: {
-    mode: 'layers',
-    content: ['./public/**/*.html', './src/**/*.vue'],
+    mode: "layers",
+    content: ["./public/**/*.html", "./src/**/*.vue"],
   },
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
